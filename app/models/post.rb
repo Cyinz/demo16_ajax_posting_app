9d892afb2a026d4eee8607f4a7684b443494074b
cyinz <13005684532@163.com>
2019-12-26 23:57:02 +0800

    通过controller实现点赞功能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   validates_presence_of :content
 
   belongs_to :user
+
+  has_many :likes, :dependent => :destroy # 级联删除，删除用户时删除该用户所有的点赞
+  has_many :liked_users, :through => :likes, :source => :user
+
+  def find_like(user)
+    self.likes.where(:user_id => user.id).first
+  end
 end
